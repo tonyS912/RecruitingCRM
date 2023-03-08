575aaacfa72671bfeba0c05e6c98d4756c700799
Tony_S <schillertony@hotmail.de>
2023-03-08 19:02:15 +0100

    Database Integreation/Implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Firespaß mit Angular 15 und TypeScript
+import { environment } from 'src/environment/environment'; //da wo unser schmutz drin ist
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app'; //damit mein angular mit firebase klar kommt
+import { provideAuth, getAuth } from '@angular/fire/auth'; //damit mein angular mit authetication klar kommt
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'; //damit mein angular firestore zeugs lesen kann
+import { provideStorage, getStorage } from '@angular/fire/storage'; //damit mein angular storage zeugs lesen kann
 
 @NgModule({
   declarations: [
@@ -12,7 +18,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), //hier wird die app initialisiert
+    provideAuth(() => getAuth()), //hier wird authetication initialisiert
+    provideFirestore(() => getFirestore()), //hier wird firestore initialisiert
+    provideStorage(() => getStorage()) //hier wird storage initialisiert
   ],
   providers: [],
   bootstrap: [AppComponent]
