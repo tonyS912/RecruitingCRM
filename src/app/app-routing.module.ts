c88791e4b2714201c42d79c8983807d32d210a9e
Tony_S <schillertony@hotmail.de>
2023-03-09 21:58:12 +0100

    do some stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { ResponseComponent } from './components/auth/response/response.component';
 import { VerificationComponent } from './components/auth/verification/verification.component';
+import { DataProtectionComponent } from './components/policity/data-protection/data-protection.component';
+import { ImprintComponent } from './components/policity/imprint/imprint.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent },
   {path: 'resetPassword', component: ForgotPasswordComponent},
   {path: 'register',  component: RegisterComponent},
   {path: 'verification', component: VerificationComponent},
-  {path: 'response', component: ResponseComponent}
+  {path: 'response', component: ResponseComponent},
+  {path: 'imprint', component: ImprintComponent},
+  {path: 'dataProtection', component: DataProtectionComponent}
 ];
 
 @NgModule({
