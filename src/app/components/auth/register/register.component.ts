731b325a955a048e3753bb154758784e49e2c129
Tony <66740164+tonyS912@users.noreply.github.com>
2023-03-10 21:05:19 +0100

    Revert "second pull request (#5)" (#6)
    This reverts commit f54c79c997766144ec2a01bbcbaa08dc697961be.

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
 
 @Component({
   selector: 'app-register',
@@ -8,18 +6,5 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
-  user = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)])
-  });
-
-  hide = true;
-
-  loginAsUser() {
-  }
-
-  loginAsGuest() {
-  }
 
 }
