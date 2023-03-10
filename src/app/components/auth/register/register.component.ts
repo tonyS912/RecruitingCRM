d8b5339c68ebb7e8b5bfce24481d7b68b3548fce
Tony_S <schillertony@hotmail.de>
2023-03-10 21:17:39 +0100

    Update same stuff from main branch

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  user = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
+  hide = true;
+
+  loginAsUser() {
+  }
+
+  loginAsGuest() {
+  }
 }
