29182d35d308bb1fc88f02b2d894eb576a66f55d
Tony_S <schillertony@hotmail.de>
2023-03-10 21:56:58 +0100

    create forgotPassword view

--- src/app/components/auth/forgot-password/forgot-password.component.ts
+++ src/app/components/auth/forgot-password/forgot-password.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent {
 
+export class ForgotPasswordComponent {
+  user = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+
+  });
+
+  hide = true;
+
+  loginAsUser() {
+  }
+
+  loginAsGuest() {
+  }
 }
