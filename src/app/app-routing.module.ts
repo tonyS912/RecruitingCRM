5625afd4ef7fcd0464a6be725ba8686906d6c7bb
Tony <66740164+tonyS912@users.noreply.github.com>
2023-03-10 14:10:07 +0100

    first pull request (#3)
    * implement main structure
    
    * update authComponent with some new components for authenticaction process
    
    * do some stuff
    
    * edit auth login comp
    
    ---------
    
    Co-authored-by: KevinSchimke <schimke.94@gmx.de>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ResponseComponent } from './components/auth/response/response.component';
+import { VerificationComponent } from './components/auth/verification/verification.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DataProtectionComponent } from './components/policity/data-protection/data-protection.component';
+import { ImprintComponent } from './components/policity/imprint/imprint.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'resetPassword', component: ForgotPasswordComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'verification', component: VerificationComponent },
+  { path: 'response', component: ResponseComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'dataProtection', component: DataProtectionComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Ng Material
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 // Firespaß mit Angular 15 und TypeScript
 import { environment } from 'src/environment/environment'; //da wo unser schmutz drin ist
@@ -10,13 +29,20 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app'; //damit m
 import { provideAuth, getAuth } from '@angular/fire/auth'; //damit mein angular mit authetication klar kommt
 import { provideFirestore, getFirestore } from '@angular/fire/firestore'; //damit mein angular firestore zeugs lesen kann
 import { provideStorage, getStorage } from '@angular/fire/storage';
+
+// Components 
+import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ImprintComponent } from './components/policity/imprint/imprint.component';
 import { DataProtectionComponent } from './components/policity/data-protection/data-protection.component';
 import { PushMessageComponent } from './components/push-message/push-message.component';
 import { StatusbarComponent } from './components/statusbar/statusbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component'; //damit mein angular storage zeugs lesen kann
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { VerificationComponent } from './components/auth/verification/verification.component';
+import { ResponseComponent } from './components/auth/response/response.component'; //damit mein angular storage zeugs lesen kann
 
 @NgModule({
   declarations: [
@@ -27,16 +53,27 @@ import { SidebarComponent } from './components/sidebar/sidebar.component'; //dam
     DataProtectionComponent,
     PushMessageComponent,
     StatusbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    ForgotPasswordComponent,
+    RegisterComponent,
+    VerificationComponent,
+    ResponseComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), //hier wird die app initialisiert
     provideAuth(() => getAuth()), //hier wird authetication initialisiert
     provideFirestore(() => getFirestore()), //hier wird firestore initialisiert
-    provideStorage(() => getStorage()) //hier wird storage initialisiert
+    provideStorage(() => getStorage()), //hier wird storage initialisiert
+    MatSidenavModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
