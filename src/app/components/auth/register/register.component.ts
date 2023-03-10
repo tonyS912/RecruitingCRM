f54c79c997766144ec2a01bbcbaa08dc697961be
Tony <66740164+tonyS912@users.noreply.github.com>
2023-03-10 21:02:44 +0100

    second pull request (#5)
    * implement main structure
    
    * update authComponent with some new components for authenticaction process
    
    * do some stuff
    
    * edit auth login comp
    
    * update register formular
    
    ---------
    
    Co-authored-by: KevinSchimke <schimke.94@gmx.de>

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  user = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  hide = true;
+
+  loginAsUser() {
+  }
+
+  loginAsGuest() {
+  }
 
 }
