c81fa46d74498b52582feefe7419c7b11e723490
Tony_S <schillertony@hotmail.de>
2023-03-12 11:47:40 +0100

    Update datasource not working yet

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,33 +6,34 @@ import { MatPaginator } from '@angular/material/paginator';
 import { Data } from '@angular/router';
 
 export interface PeriodicElement {
-  name: string;
+  firstName: string;
+  lastName: string;
   position: number;
-  gewicht: number;
-  symbol: string;
+  job: string;
+  termin: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', gewicht: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', gewicht: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', gewicht: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', gewicht: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', gewicht: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', gewicht: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', gewicht: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', gewicht: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', gewicht: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', gewicht: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', gewicht: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', gewicht: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', gewicht: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', gewicht: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', gewicht: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', gewicht: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', gewicht: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', gewicht: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', gewicht: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', gewicht: 40.078, symbol: 'Ca'},
+  {position: 1, firstName: 'Nora', lastName: 'Johnson', job: 'Developer', termin: '03.12.2023'},
+  {position: 2, firstName: 'Jessie', lastName: 'Lucas', job: 'Junior-Developer', termin: '03.12.2023'},
+  {position: 3, firstName: 'Clifford', lastName: 'White', job: 'Developer', termin: '03.12.2023'},
+  {position: 4, firstName: 'Jesse', lastName: 'Barnett', job: 'Developer', termin: '03.12.2023'},
+  {position: 5, firstName: 'Ron', lastName: 'Hansen', job: 'Developer', termin: '03.12.2023'},
+  {position: 6, firstName: 'Eva', lastName: 'Burns', job: 'Developer', termin: '03.12.2023'},
+  {position: 7, firstName: 'Jar', lastName: 'Coleman', job: 'Developer', termin: '03.12.2023'},
+  {position: 8, firstName: 'Luke', lastName: 'Kuhn', job: 'Developer', termin: '03.12.2023'},
+  {position: 9, firstName: 'Terry', lastName: 'Smith', job: 'Developer', termin: '03.12.2023'},
+  {position: 10, firstName: 'Calvin', lastName: 'Dunn', job: 'Developer', termin: '03.12.2023'},
+  {position: 11, firstName: 'Kelly', lastName: 'Butler', job: 'Developer', termin: '03.12.2023'},
+  {position: 12, firstName: 'Dwayne', lastName: 'Green', job: 'Developer', termin: '03.12.2023'},
+  {position: 13, firstName: 'Sylvia', lastName: 'Sutton', job: 'Developer', termin: '03.12.2023'},
+  {position: 14, firstName: 'Anita', lastName: 'Webb', job: 'Developer', termin: '03.12.2023'},
+  {position: 15, firstName: 'Roger', lastName: 'Stevens', job: 'Developer', termin: '03.12.2023'},
+  {position: 16, firstName: 'Kristin', lastName: 'Olson', job: 'Developer', termin: '03.12.2023'},
+  {position: 17, firstName: 'Gladys', lastName: 'Morales', job: 'Developer', termin: '03.12.2023'},
+  {position: 18, firstName: 'Anne', lastName: 'Daniels', job: 'Developer', termin: '03.12.2023'},
+  {position: 19, firstName: 'Wayne', lastName: 'Smith', job: 'Developer', termin: '03.12.2023'},
+  {position: 20, firstName: 'Harry', lastName: 'Campbell', job: 'Developer', termin: '03.12.2023'},
 ];
 @Component({
   selector: 'app-dashboard',
@@ -41,7 +42,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class DashboardComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'gewicht', 'symbol'];
+  displayedColumns: string[] = ['position', 'firstName', 'lastName', 'job', 'termin'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   constructor(private _liveAnnouncer: LiveAnnouncer) {
